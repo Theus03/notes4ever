1ff1b1a0670dc58372fe1f11591ea2dc4c6af93a
kaua vitorio <kauavitorioof@gmail.com>
2021-04-03 13:38:33 -0300

    first commit

---
+++ app/src/main/java/co/ex/notesforever/Activitys/MainActivity.java
@@ -0,0 +1,16 @@
+package co.ex.notesforever.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.ex.notesforever.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
